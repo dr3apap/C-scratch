e256653c582787cbf3ec4c082a02fe81307384b7
Adebola Adesina <dr3apap@gmail.com>
2022-06-16 13:25:37 -0700

    Fixed the bug in max function

--- debug.c
+++ debug.c
@@ -14,6 +14,7 @@ int main(void)
     i = 10;
     h = 0;
     if (max(i, h) != i) printf("What even is life?\n");
+    printf("All seem to be working fine!\n");
     
     return EXIT_SUCCESS;
 
@@ -21,7 +22,7 @@ int main(void)
 
 uint32_t max(uint32_t num1, uint32_t num2)
 {   
-    uint32_t result;
+    uint32_t result = 0;
     if (num1 > result) result =  num1;
     if (num2 > result) result = num2;
     return result;
