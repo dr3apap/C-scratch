2311badff73050205b791007e1566c717f2302fc
Adebola Adesina <dr3apap@gmail.com>
2022-06-22 09:20:31 -0700

    implement reading from a file

--- cat.c
+++ cat.c
@@ -1,16 +1,30 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 
 #define BUFFER_SIZE 80
 char buffer[BUFFER_SIZE];
 
-int main()
+int main(int argc, char *argv[])
 {
     ssize_t len;
-   while ((len =  read(STDIN_FILENO, buffer, BUFFER_SIZE))){;
+    int fd = STDIN_FILENO;
+
+     if (argc > 1){
+         fd = open(argv[1], O_RDONLY);
+
+         if (fd == -1){
+             write(STDERR_FILENO, "no file\n", 8);
+             exit(EXIT_FAILURE);
+         }
+     }
+
+   while ((len =  read(fd, buffer, BUFFER_SIZE))){;
    write(STDOUT_FILENO, buffer, len);
 
    }
+
+   close(fd);
     
 }
